03e354c7cdbecff8b2d65d3fe321943295859d30
KUGAKU <githubgemini.0521@gmail.com>
2019-05-16 14:14:48 +0900

    データベース保存処理とリソース名変更

--- app/src/main/java/com/example/atwatch/Main2Activity.kt
+++ app/src/main/java/com/example/atwatch/Main2Activity.kt
@@ -1,12 +1,37 @@
 package com.example.atwatch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import io.realm.Realm
+import io.realm.kotlin.createObject
+import io.realm.kotlin.where
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main2Activity : AppCompatActivity() {
+    private lateinit var realm: Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        realm = Realm.getDefaultInstance()
+
+        save_button.setOnClickListener { view: View ->
+            realm.executeTransaction { db: Realm ->
+                val maxId = db.where<Contest>().max("id")
+                val nextId = (maxId?.toLong() ?: 0L) + 1
+                val contest = db.createObject<Contest>(nextId)
+                contest.title = contesttitle.text.toString()
+            }
+        }
+
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
     }
 }
