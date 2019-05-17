d5216f79b8134c73acb6bc4e0595b47aa9604b41
test <test>
2019-05-17 09:42:07 +0900

    kari
    kari

--- app/src/main/java/com/example/atwatch/Question.kt
+++ app/src/main/java/com/example/atwatch/Question.kt
@@ -1,8 +1,10 @@
 package com.example.atwatch
 
 import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
 
 open class Question : RealmObject(){
+    @PrimaryKey
     var question_id: Long = 0
     var question: String = ""
     var level: String = ""
