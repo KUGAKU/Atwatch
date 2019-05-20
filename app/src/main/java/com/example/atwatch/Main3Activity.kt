6beec5cf97711f4399fc658a327feb516ca680f1
test <test>
2019-05-20 12:08:25 +0900

    カウント処理の実装
    ストップ処理とかがまだです

--- app/src/main/java/com/example/atwatch/Main3Activity.kt
+++ app/src/main/java/com/example/atwatch/Main3Activity.kt
@@ -2,11 +2,48 @@ package com.example.atwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import kotlinx.android.synthetic.main.activity_main3.*
 
 class Main3Activity : AppCompatActivity() {
+    val handler = Handler()
+    var timeValue = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+        val runnable = object : Runnable {
+            override fun run() {
+                timeValue++
+                timeToText(timeValue)?.let { //timeToTextで表示データを作る
+                    timeText.text = it //作った表示データに更新
+                }
+                handler.postDelayed(this, 1000) //1秒ごとにpost
+            }
+        }
+
+        countUpStart_button.setOnClickListener {
+            handler.post(runnable)
+
+        }
+
+        //cancel_button.setOnClickListener {
+        //
+        //}
+    }
+
+
+    private fun timeToText(time: Int = 0): String? {
+        return if (time < 0) {
+            null
+        } else if (time == 0) {
+            "00:00:00"
+        } else {
+            val h = time / 3600
+            val m = time % 3600 / 60
+            val s = time % 60
+            "%1$02d:%2$02d:%3$02d".format(h, m, s) // 整形
+        }
     }
 }
